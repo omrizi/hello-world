c6de992555efd1eec3abd0f839b4dfaf35e0f0cf
Omri <45109481+omrizi@users.noreply.github.com>
2025-03-04 06:20:32 +0200

    add hello world (#3)
    * add hello world
    
    * add dispatch flow

---
+++ main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+def main():
+    try:
+        env = os.environ.get("ENV"):
+        tier = os.environ.get("TIER"):
+        print(f"{env}-{tier}")
+    except Exception as e:
+        print(f"error {e} occured")
+
+if __name__ == "__main__":
+    main()
