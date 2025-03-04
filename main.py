db31ca654effaf484d158175fb879a28cd462022
Omri <45109481+omrizi@users.noreply.github.com>
2025-03-04 06:21:35 +0200

    bug fix

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 load_dotenv()
 def main():
     try:
-        env = os.environ.get("ENV"):
-        tier = os.environ.get("TIER"):
+        env = os.environ.get("ENV")
+        tier = os.environ.get("TIER")
         print(f"{env}-{tier}")
     except Exception as e:
         print(f"error {e} occured")
